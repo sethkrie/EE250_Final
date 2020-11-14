90f7a196114d78b34f3c9b6bd48e62bda2c10ec5
Seth Krieger <sethkrie@usc.edu>
2020-11-14 13:52:29 -0800

    ..

--- rpi_pub_sub_test_in.py
+++ rpi_pub_sub_test_in.py
@@ -110,7 +110,7 @@ if __name__ == '__main__':
         # Look at the averge of the moving window across 10s
         # Publish user's average distance over 10 seconds sampled at 20Hz to /users
         avg = numpy.cumsum(avg_distance) / len(distance_window)      
-        client.publish("P2P/users", avg)
+        client.publish("P2P/users", int(avg))
         avg_distance.clear()
         
 
