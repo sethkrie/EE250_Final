36b470bc2ed05a641272098dc73b92f4d3008f54
Seth Krieger <sethkrie@usc.edu>
2020-11-14 13:06:52 -0800

    more syntax stuff

--- usr_dsp.py
+++ usr_dsp.py
@@ -14,7 +14,6 @@ ultPrt = 4 # D4 is the port for ultrasonic ranger
 t = 10
 fs = 20
 ts = np.linspace(0, 10, ((1/fs) * t)
-fig = plt.figure()
 
 distance_window = [(1/fs) * t]
 for count in distance_window:
@@ -26,7 +25,7 @@ for count in distance_window:
 # Publish user's average distance over 10 seconds sampled at 20Hz to /users
 avg_distance = sum(distance_window[:]) / len(distance_window)
 
-fig.scatter(ts, distance_window) 
-fig.scatter(ts, avg_distance)
-fig.show() 
+plt.scatter(ts, distance_window) 
+plt.scatter(ts, avg_distance)
+plt.show() 
 
